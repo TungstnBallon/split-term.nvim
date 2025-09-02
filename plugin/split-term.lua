9354d52adb9f81fb3a2a09ef05b06b33a6402b37
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-09-02 09:12:33 +0200

    style: follow example from new |lua-plugin-guide|

--- plugin/split-term.lua
+++ plugin/split-term.lua
@@ -1,3 +1,3 @@
-vim.keymap.set({ "n", "i", "v", "o", "t" }, "<Plug>SplitTermToggle", function()
+vim.keymap.set({ "n", "i", "v", "o", "t" }, "<Plug>(SplitTermToggle)", function()
 	require("split-term").toggle()
 end)
